cded67b58b16d9813bfe93b295f7c6fe87c70b52
Chip <Panopticon772@gmail.com>
2018-09-27 23:48:18 -0400

    Added grid band section.

--- app/assets/scripts/pt.js
+++
@@ -1,9 +0,0 @@
-// toggle hamburger menu
-const toggleLinks = () => {
-    const getNav = document.getElementById("my-top-nav");
-    if (getNav.className === "top-nav") {
-        getNav.className += "responsive";
-    } else {
-        getNav.className = "top-nav";
-    }
-}
